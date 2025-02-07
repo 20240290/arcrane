67b6aeb7513dc4488ea84da1d938c7755444c48a
Joseph <joseph20240290@gmail.com>
2025-02-06 21:45:04 -0400

    working on companion script to handle the second raspberry pi

--- companion/companion.py
+++ companion/companion.py
@@ -18,9 +18,20 @@ from gpiozero import Servo
 from gpiozero import OutputDevice
 from gpiozero import Button
 import time
-import MqttClient
 from signal import pause
 from adafruit_servokit import ServoKit
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from MqttClient import MqttClient
+
+claw = 0  # claw channel
+claw_rotation = 1 # claw rotation channel
+
+# Initialize PCA9685 (16-channel PWM)
+kit = ServoKit(channels=16)
 
 #Claw action motor
 def moveMotor(movement):
@@ -70,10 +81,20 @@ def process_message(msg):
     print(f"Received: {msg.payload.decode()} on topic {msg.topic}")
     #loop(msg.payload.decode())
 
+def gradual_move_servo(channel, target_angle, step=1, delay=0.01):
+    """Gradually moves a servo to the target angle."""
+    current_angle = kit.servo[channel].angle or 0  # Get current angle or default to 0
+    step = step if target_angle > current_angle else -step  # Adjust step direction
+    
+    for angle in range(int(current_angle), target_angle + step, step):
+        kit.servo[channel].angle = angle
+        time.sleep(delay)
+
+
 def main():
     print ('Program is starting...')
     try:
-        client = MqttClient.MqttClient("localhost","raspberry/signal")
+        client = MqttClient("localhost","raspberry/signal")
         client.on_message = on_message
         client.subscribe_to_topic()
 
